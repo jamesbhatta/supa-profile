bd04838e347aceb33a6458f1b7ad6f71a67292c3
bishal221973 <bishalchaudhary2018bca@gmail.com>
2022-06-01 11:23:54 +0545

    unit 8 table

--- public/js/resources_js_pages_state-of-agricultural-sector_Index_vue.js
+++ public/js/resources_js_pages_state-of-agricultural-sector_Index_vue.js
@@ -1,6 +1,46 @@
 "use strict";
 (self["webpackChunk"] = self["webpackChunk"] || []).push([["resources_js_pages_state-of-agricultural-sector_Index_vue"],{
 
+/***/ "./node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/pages/state-of-agricultural-sector/Index.vue?vue&type=script&lang=js&":
+/*!************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/pages/state-of-agricultural-sector/Index.vue?vue&type=script&lang=js& ***!
+  \************************************************************************************************************************************************************************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
+  data: function data() {
+    return {
+      businessindustrydata: {
+        title: "ठूला तथा मझौला उद्योगहरुको प्रदेशगत विवरण",
+        labels: ["प्रदेश", "कृषि र वन", "निर्माण", "उर्जा", "सञ्चार", "उत्पादनमा आधारित", "खानी", "सेवा", "पर्यटन", "उद्योगका संख्या", "कूल लगानी (रु.दश लाखमा)", "रोजगारी"],
+        data: [["प्रदेश १", 114, 3, 77, 2, 369, 2, 97, 50, 714, 37770.5, 76264], ["मधेश", 24, 1, 1, 1, 418, 3, 56, 18, 522, 10144.4, 54646], ["बागमती", 261, 48, 135, 45, 1695, 9, 1804, 1301, 5298, 70519.3, 357493], ["गण्डकी", 32, 4, 90, 1, 57, 9, 110, 253, 556, 28538.7, 29491], ["लुम्बिनी", 19, 2, 13, 1, 390, 24, 70, 69, 588, 20165.6, 60682], ["कर्णाली", 0, 0, 13, 0, 6, 5, 9, 6, 39, 11394.6, 2606], ["सुदूरपश्चिम", 6, 0, 21, 0, 41, 17, 22, 8, 115, 3843.1, 9919], ["जम्मा", 456, 58, 350, 50, 2976, 69, 2168, 1705, 7832, 182376.3, 591101]]
+      },
+      minesdata: {
+        title: "सुदूरपश्चिममा रहेका मुख्य खानीको विवरण",
+        labels: ["क्र.स.", "खानी", "जिल्ला", "अवस्था"],
+        data: [[1, "तामा", "दार्चुला, डडेलधुरा, बझाङ र बाजुरा", "देखिएको, परम्परागत रुपमा निकालिएको, भेटिएको"], [2, "सुन", "डोटी र बैतडी,बझाङ, दार्चुला,डडेलधुरा, कैलाली, बाजुरा, अछाम", "प्रारम्भिर रुपमा देखिएको,संकलन तथा सम्भावना परीक्षण"], [3, "फलाम", "बैतडी, बझाङ र दार्चुला", "परम्परागत रुपमा निकालिएको,देखिएका"], [4, "सिसा", "बझाङ, बैतडी, अछाम, बाजुरा र डडेलधुरा", 15.3], [5, "लिथियम", "डोटी र बझाङ", "देखिएको"], [6, "टिन", "डडेलधुरा, डोटी र बझाङ", "देखिएको, भेटिएको"], [7, "युरेनियम", "बैतडी, दार्चुला, बझाङ, बाजुरा", "देखिएको"], [8, "जिंक", "बझाङ, बैतडी र डडेलधुरा", "देखिएको"], [9, "आर्सेनिक", "डेडेलधुरा र बझाङ", "देखिएको"], [10, "क्रोमियम", "बझाङ", "देखिएको"], [11, "कोबाल्ट", "बझाङ", "देखिएको"], [12, "चुनढुंगा", "बैतडी", " "]]
+      }
+    };
+  }
+});
+
+/***/ }),
+
 /***/ "./resources/js/pages/state-of-agricultural-sector/Index.vue":
 /*!*******************************************************************!*\
   !*** ./resources/js/pages/state-of-agricultural-sector/Index.vue ***!
@@ -11,26 +51,103 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
-/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! !../../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
-var render, staticRenderFns
-var script = {}
+/* harmony import */ var _Index_vue_vue_type_template_id_0152cfe6_scoped_true___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Index.vue?vue&type=template&id=0152cfe6&scoped=true& */ "./resources/js/pages/state-of-agricultural-sector/Index.vue?vue&type=template&id=0152cfe6&scoped=true&");
+/* harmony import */ var _Index_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./Index.vue?vue&type=script&lang=js& */ "./resources/js/pages/state-of-agricultural-sector/Index.vue?vue&type=script&lang=js&");
+/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! !../../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+
+
+
 
 
 /* normalize component */
 ;
-var component = (0,_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_0__["default"])(
-  script,
-  render,
-  staticRenderFns,
+var component = (0,_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__["default"])(
+  _Index_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__["default"],
+  _Index_vue_vue_type_template_id_0152cfe6_scoped_true___WEBPACK_IMPORTED_MODULE_0__.render,
+  _Index_vue_vue_type_template_id_0152cfe6_scoped_true___WEBPACK_IMPORTED_MODULE_0__.staticRenderFns,
   false,
   null,
-  null,
+  "0152cfe6",
   null
   
 )
 
+/* hot reload */
+if (false) { var api; }
 component.options.__file = "resources/js/pages/state-of-agricultural-sector/Index.vue"
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (component.exports);
+
+/***/ }),
+
+/***/ "./resources/js/pages/state-of-agricultural-sector/Index.vue?vue&type=script&lang=js&":
+/*!********************************************************************************************!*\
+  !*** ./resources/js/pages/state-of-agricultural-sector/Index.vue?vue&type=script&lang=js& ***!
+  \********************************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_babel_loader_lib_index_js_clonedRuleSet_5_0_rules_0_use_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Index_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!../../../../node_modules/vue-loader/lib/index.js??vue-loader-options!./Index.vue?vue&type=script&lang=js& */ "./node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/pages/state-of-agricultural-sector/Index.vue?vue&type=script&lang=js&");
+ /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (_node_modules_babel_loader_lib_index_js_clonedRuleSet_5_0_rules_0_use_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Index_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__["default"]); 
+
+/***/ }),
+
+/***/ "./resources/js/pages/state-of-agricultural-sector/Index.vue?vue&type=template&id=0152cfe6&scoped=true&":
+/*!**************************************************************************************************************!*\
+  !*** ./resources/js/pages/state-of-agricultural-sector/Index.vue?vue&type=template&id=0152cfe6&scoped=true& ***!
+  \**************************************************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "render": () => (/* reexport safe */ _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_Index_vue_vue_type_template_id_0152cfe6_scoped_true___WEBPACK_IMPORTED_MODULE_0__.render),
+/* harmony export */   "staticRenderFns": () => (/* reexport safe */ _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_Index_vue_vue_type_template_id_0152cfe6_scoped_true___WEBPACK_IMPORTED_MODULE_0__.staticRenderFns)
+/* harmony export */ });
+/* harmony import */ var _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_Index_vue_vue_type_template_id_0152cfe6_scoped_true___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!../../../../node_modules/vue-loader/lib/index.js??vue-loader-options!./Index.vue?vue&type=template&id=0152cfe6&scoped=true& */ "./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/pages/state-of-agricultural-sector/Index.vue?vue&type=template&id=0152cfe6&scoped=true&");
+
+
+/***/ }),
+
+/***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/pages/state-of-agricultural-sector/Index.vue?vue&type=template&id=0152cfe6&scoped=true&":
+/*!*****************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/pages/state-of-agricultural-sector/Index.vue?vue&type=template&id=0152cfe6&scoped=true& ***!
+  \*****************************************************************************************************************************************************************************************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "render": () => (/* binding */ render),
+/* harmony export */   "staticRenderFns": () => (/* binding */ staticRenderFns)
+/* harmony export */ });
+var render = function () {
+  var _vm = this
+  var _h = _vm.$createElement
+  var _c = _vm._self._c || _h
+  return _c(
+    "div",
+    { staticClass: "container py-5" },
+    [
+      _c("h1", { staticClass: "page-title" }, [_vm._v("कृषि क्षेत्र")]),
+      _vm._v(" "),
+      _c("data-viewer", { attrs: { data: _vm.businessindustrydata } }),
+      _vm._v(" "),
+      _c("div", { staticClass: "my-5" }),
+      _vm._v(" "),
+      _c("data-viewer", { attrs: { data: _vm.minesdata } }),
+      _vm._v(" "),
+      _c("div", { staticClass: "my-5" }),
+      _vm._v(" "),
+      _c("data-viewer", { attrs: { data: _vm.districtWiseAreaOfStateData } }),
+    ],
+    1
+  )
+}
+var staticRenderFns = []
+render._withStripped = true
+
+
 
 /***/ })
 
