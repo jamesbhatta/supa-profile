7ce4096707606e4eef0993cb9c89a8a0cb4274cd
bishal221973 <bishalchaudhary2018bca@gmail.com>
2022-06-01 17:22:17 +0545

    unit 11 last table

--- public/js/resources_js_pages_miscellaneous_Index_vue.js
+++ public/js/resources_js_pages_miscellaneous_Index_vue.js
@@ -1,6 +1,41 @@
 "use strict";
 (self["webpackChunk"] = self["webpackChunk"] || []).push([["resources_js_pages_miscellaneous_Index_vue"],{
 
+/***/ "./node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/pages/miscellaneous/Index.vue?vue&type=script&lang=js&":
+/*!*********************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/pages/miscellaneous/Index.vue?vue&type=script&lang=js& ***!
+  \*********************************************************************************************************************************************************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
+  data: function data() {
+    return {
+      territorialGeographicalAreaData: {
+        title: "प्रदेश सरकार मातहतका मन्त्रालय, आयोग, निर्देशनालय र कार्यालयको विवरण",
+        labels: ["क्र.स.", "कार्यायलको नाम", "सम्पर्क नम्बर", "इमेल"],
+        data: [[1, "प्रदेश प्रमुखको कार्यालय", "०९१–५२४२७३", "ophsudurpaschim@gmail.com"], [2, "मुख्यमन्त्री तथा मन्त्रिपरिषद्को कार्यालय", "", "admin.ocmpm@gmail.com"], [3, "आर्थिक मामिला मन्त्रालय", "", "moeap7@gmail.com"], [4, "भौतिक पूर्वाधार विकास मन्त्रालय", "", "mopidp7@gmail.com"], [5, "उद्योग पर्यटन वन तथा वातावरण मन्त्रालय", "", "moitfesudurpaschim@gmail.com"], [6, "सामाजिक विकास मन्त्रालय", "", "mosdkailali07@gmail.com"], [7, "भुमिब्यवस्था कृषि तथा सहकारी मन्त्रालय", "", "molmac7@gmail.com"], [8, "आन्तरिक मामिला तथा कानुन मन्त्रालय", "", "moiaffairsandlaw7@gmail.com"], [9, "प्रदेश सभा सचिवालय", "", "pradeshsabha7@gmail.com"], [10, "लोकसेवा आयोग", "", "ppsc.p7@gmail.com"], [11, "प्रदेश नीति तथा योजना आयोग", "", "pppc.supa@gmail.com"], [12, "प्रदेश लेखा नियन्त्रक कार्यालय", "", "pfcop7@gmail.com"], [13, "सामाजिक विकास मन्त्रालय मातहका कार्यालयहरु", "", ""], [14, "शिक्षा विकास निर्देशनालय", "", ""], [15, "स्वास्थ्य निर्देशनालय", "", ""], [16, "आयुर्वेद चिकित्सा प्रतिष्ठान", "", ""]]
+      }
+    };
+  }
+});
+
+/***/ }),
+
 /***/ "./resources/js/pages/miscellaneous/Index.vue":
 /*!****************************************************!*\
   !*** ./resources/js/pages/miscellaneous/Index.vue ***!
@@ -11,26 +46,105 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
-/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! !../../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
-var render, staticRenderFns
-var script = {}
+/* harmony import */ var _Index_vue_vue_type_template_id_28d68e10_scoped_true___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Index.vue?vue&type=template&id=28d68e10&scoped=true& */ "./resources/js/pages/miscellaneous/Index.vue?vue&type=template&id=28d68e10&scoped=true&");
+/* harmony import */ var _Index_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./Index.vue?vue&type=script&lang=js& */ "./resources/js/pages/miscellaneous/Index.vue?vue&type=script&lang=js&");
+/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! !../../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+
+
+
 
 
 /* normalize component */
 ;
-var component = (0,_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_0__["default"])(
-  script,
-  render,
-  staticRenderFns,
+var component = (0,_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__["default"])(
+  _Index_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__["default"],
+  _Index_vue_vue_type_template_id_28d68e10_scoped_true___WEBPACK_IMPORTED_MODULE_0__.render,
+  _Index_vue_vue_type_template_id_28d68e10_scoped_true___WEBPACK_IMPORTED_MODULE_0__.staticRenderFns,
   false,
   null,
-  null,
+  "28d68e10",
   null
   
 )
 
+/* hot reload */
+if (false) { var api; }
 component.options.__file = "resources/js/pages/miscellaneous/Index.vue"
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (component.exports);
+
+/***/ }),
+
+/***/ "./resources/js/pages/miscellaneous/Index.vue?vue&type=script&lang=js&":
+/*!*****************************************************************************!*\
+  !*** ./resources/js/pages/miscellaneous/Index.vue?vue&type=script&lang=js& ***!
+  \*****************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_babel_loader_lib_index_js_clonedRuleSet_5_0_rules_0_use_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Index_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!../../../../node_modules/vue-loader/lib/index.js??vue-loader-options!./Index.vue?vue&type=script&lang=js& */ "./node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/pages/miscellaneous/Index.vue?vue&type=script&lang=js&");
+ /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (_node_modules_babel_loader_lib_index_js_clonedRuleSet_5_0_rules_0_use_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Index_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__["default"]); 
+
+/***/ }),
+
+/***/ "./resources/js/pages/miscellaneous/Index.vue?vue&type=template&id=28d68e10&scoped=true&":
+/*!***********************************************************************************************!*\
+  !*** ./resources/js/pages/miscellaneous/Index.vue?vue&type=template&id=28d68e10&scoped=true& ***!
+  \***********************************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "render": () => (/* reexport safe */ _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_Index_vue_vue_type_template_id_28d68e10_scoped_true___WEBPACK_IMPORTED_MODULE_0__.render),
+/* harmony export */   "staticRenderFns": () => (/* reexport safe */ _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_Index_vue_vue_type_template_id_28d68e10_scoped_true___WEBPACK_IMPORTED_MODULE_0__.staticRenderFns)
+/* harmony export */ });
+/* harmony import */ var _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_Index_vue_vue_type_template_id_28d68e10_scoped_true___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!../../../../node_modules/vue-loader/lib/index.js??vue-loader-options!./Index.vue?vue&type=template&id=28d68e10&scoped=true& */ "./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/pages/miscellaneous/Index.vue?vue&type=template&id=28d68e10&scoped=true&");
+
+
+/***/ }),
+
+/***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/pages/miscellaneous/Index.vue?vue&type=template&id=28d68e10&scoped=true&":
+/*!**************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/pages/miscellaneous/Index.vue?vue&type=template&id=28d68e10&scoped=true& ***!
+  \**************************************************************************************************************************************************************************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "render": () => (/* binding */ render),
+/* harmony export */   "staticRenderFns": () => (/* binding */ staticRenderFns)
+/* harmony export */ });
+var render = function () {
+  var _vm = this
+  var _h = _vm.$createElement
+  var _c = _vm._self._c || _h
+  return _c(
+    "div",
+    { staticClass: "container py-5" },
+    [
+      _c("h1", { staticClass: "page-title" }, [_vm._v("विविध")]),
+      _vm._v(" "),
+      _c("data-viewer", { attrs: { data: _vm.geographicalAreaData } }),
+      _vm._v(" "),
+      _c("div", { staticClass: "my-5" }),
+      _vm._v(" "),
+      _c("data-viewer", {
+        attrs: { data: _vm.territorialGeographicalAreaData },
+      }),
+      _vm._v(" "),
+      _c("div", { staticClass: "my-5" }),
+      _vm._v(" "),
+      _c("data-viewer", { attrs: { data: _vm.districtWiseAreaOfStateData } }),
+    ],
+    1
+  )
+}
+var staticRenderFns = []
+render._withStripped = true
+
+
 
 /***/ })
 
---
+++ resources/js/pages/miscellaneous/Index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container py-5">
+    <h1 class="page-title">विविध</h1>
+    <data-viewer :data="geographicalAreaData"></data-viewer>
+    <div class="my-5"></div>
+    <data-viewer :data="territorialGeographicalAreaData"></data-viewer>
+    <div class="my-5"></div>
+    <data-viewer :data="districtWiseAreaOfStateData"></data-viewer>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+      territorialGeographicalAreaData: {
+        title: "प्रदेश सरकार मातहतका मन्त्रालय, आयोग, निर्देशनालय र कार्यालयको विवरण",
+        labels: ["क्र.स.", "कार्यायलको नाम", "सम्पर्क नम्बर", "इमेल"],
+        data: [
+          [1, "प्रदेश प्रमुखको कार्यालय", "०९१–५२४२७३","ophsudurpaschim@gmail.com"],
+          [2, "मुख्यमन्त्री तथा मन्त्रिपरिषद्को कार्यालय", "","admin.ocmpm@gmail.com"],
+          [3, "आर्थिक मामिला मन्त्रालय", "","moeap7@gmail.com"],
+          [4, "भौतिक पूर्वाधार विकास मन्त्रालय", "","mopidp7@gmail.com"],
+          [5, "उद्योग पर्यटन वन तथा वातावरण मन्त्रालय", "", "moitfesudurpaschim@gmail.com"],
+          [6, "सामाजिक विकास मन्त्रालय", "", "mosdkailali07@gmail.com"],
+          [7, "भुमिब्यवस्था कृषि तथा सहकारी मन्त्रालय", "","molmac7@gmail.com"],
+          [8, "आन्तरिक मामिला तथा कानुन मन्त्रालय", "","moiaffairsandlaw7@gmail.com"],
+          [9, "प्रदेश सभा सचिवालय", "","pradeshsabha7@gmail.com"],
+          [10, "लोकसेवा आयोग", "","ppsc.p7@gmail.com"],
+          [11, "प्रदेश नीति तथा योजना आयोग", "","pppc.supa@gmail.com"],
+          [12, "प्रदेश लेखा नियन्त्रक कार्यालय", "","pfcop7@gmail.com"],
+          [13, "सामाजिक विकास मन्त्रालय मातहका कार्यालयहरु", "",""],
+          [14, "शिक्षा विकास निर्देशनालय", "",""],
+          [15, "स्वास्थ्य निर्देशनालय", "",""],
+          [16, "आयुर्वेद चिकित्सा प्रतिष्ठान", "",""],
+          
+        ],
+      },
+
+    };
+  },
+};
+</script>
+
+
+<style scoped>
+</style>
