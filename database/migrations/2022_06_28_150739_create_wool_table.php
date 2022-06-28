845a7776f7712a4c88de24613d7028e480832065
bishal221973 <bishalchaudhary2018bca@gmail.com>
2022-06-28 15:28:23 +0545

    उन उत्पादनको अवस्था CRUD

---
+++ database/migrations/2022_06_28_150739_create_wool_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWoolTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wool', function (Blueprint $table) {
+            $table->id();
+             $table->string('district');
+            $table->integer('sheep');
+            $table->integer('wool');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wool');
+    }
+}
