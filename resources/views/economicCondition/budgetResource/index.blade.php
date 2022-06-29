949456e224e796eb17a8ebadf83acb9609128811
bishal221973 <bishalchaudhary2018bca@gmail.com>
2022-06-29 07:44:46 +0545

    wrp

--- resources/views/economicCondition/budgetResource/index.blade.php
+++ resources/views/economicCondition/budgetResource/index.blade.php
@@ -1,103 +1,116 @@
 @extends('layouts.app')
 
 @section('content')
-
-<div class="container">
-    @include('alerts.all')
-</div>
-<div class="container">
-    <div class="card z-depth-0">
-        <div class="col-12">
-            <label class="col-12 text-center font-weight-bold h4 my-5">बजेटको स्रोतको अवस्था</label>
-            <hr>
+    <div class="container-fluid">
+        <h3 class="font-weight-bold">बजेटको स्रोतको अवस्था</h3>
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
+                <li class="breadcrumb-item active" aria-current="page">आर्थिक अवस्था</li>
+                <li class="breadcrumb-item active" aria-current="page">बजेटको स्रोतको अवस्था</li>
+            </ol>
+        </nav>
+        <div class="container">
+            @include('alerts.all')
         </div>
-        <div class="card-body">
-            <form action="{{$budgetResource->id? route('budget-resource.update',$budgetResource):route('budget-resource.store')}}" method="POST" class="form">
-                @csrf
-               @isset($budgetResource->id)
-                   @method('PUT')
-               @endisset
-               
-                <div class="form-group">
-                    <label for="input-name">आय तथा राजश्व</label>
-                    <input type="text" id="input-name" name="income" class="form-control" autocomplete="off" value="{{old('income',$budgetResource->income)}}">
+        <div class="card z-depth-0">
+            <div class="card-header">
+                <div style="overflow: auto;scrollbar-width: none;">
+                    <div>
+                        <nav class="nav nav-pills" id="pills-tab" role="tablist">
+                            <h4>बजेटको स्रोतको अवस्था</h4>
+                        </nav>
+                    </div>
                 </div>
-                <div class="form-group">
-                    <label for="input-name">रकम (रु.हजारमा)</label>
-                    <input type="text" id="input-name" name="price" class="form-control" autocomplete="off" value="{{old('price',$budgetResource->price)}}">
-                </div>
-               
-                <div class="form-group">
-                    <button type="submit" class="btn btn-success z-depth-0">{{$budgetResource->id? 'Update':'सेभ गर्नुहोस्'}} </button>
-                </div>
-            </form>
+            </div>
+            <div class="card-body">
+                <form
+                    action="{{ $budgetResource->id ? route('budget-resource.update', $budgetResource) : route('budget-resource.store') }}"
+                    method="POST" class="form">
+                    @csrf
+                    @isset($budgetResource->id)
+                        @method('PUT')
+                    @endisset
+
+                    <div class="row">
+                        <div class="form-group col-lg-6">
+                            <label for="input-name">आय तथा राजश्व</label>
+                            <input type="text" id="input-name" name="income" class="form-control" autocomplete="off"
+                                value="{{ old('income', $budgetResource->income) }}">
+                        </div>
+                        <div class="form-group col-lg-6">
+                            <label for="input-name">रकम (रु.हजारमा)</label>
+                            <input type="text" id="input-name" name="price" class="form-control" autocomplete="off"
+                                value="{{ old('price', $budgetResource->price) }}">
+                        </div>
+
+
+                    </div>
+                    <div class="form-group">
+                        <button type="submit"
+                            class="btn btn-success z-depth-0">{{ $budgetResource->id ? 'Update' : 'सेभ गर्नुहोस्' }} </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+        <div class="my-4"></div>
+
+        <div class="card z-depth-0">
+            <div class="card-header">
+                <h1 class="h3-responsive d-inline-block">बजेटको स्रोतको अवस्था</h1>
+                {{-- <small>(हाल {{ count($schools)  }}  विद्यालय {{ count($schools) > 1 ? 'हरु छन्' : 'छ' }} )</small> --}}
+
+            </div>
+            <div class="card-body">
+                <table class="table table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th>क्र.स.</th>
+                            <th>विवरण</th>
+                            <th>सूचक (प्रतिशत)</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse ($budgetResources as $item)
+                            <tr>
+                                <td>{{ $loop->iteration }}</td>
+                                <td>{{ $item->income }}</td>
+                                <td>{{ $item->price }}</td>
+                                <td>
+                                    <a class="action-btn text-primary"
+                                        href="{{ route('budget-resource.edit', $item) }}"><i class="far fa-edit"></i></a>
+                                    <form action="{{ route('budget-resource.destroy', $item) }}" method="post"
+                                        onsubmit="return confirm('के तपाईँ निश्चित हुनुहुन्छ?')"
+                                        class="form-inline d-inline">
+                                        @csrf
+                                        @method('delete')
+                                        <button type="submit" class="action-btn text-danger"><i
+                                                class="far fa-trash-alt"></i></button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @empty
+                            <tr class="text-center font-italic">
+                                <td colspan="10">* * डाटाबेसमा कुनै डाटा छैन * *</td>
+                            </tr>
+                        @endforelse
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
-
-    <div class="my-4"></div>
-
-    <div class="card z-depth-0">
-        <div class="card-header">
-            <h1 class="h3-responsive d-inline-block">बजेटको स्रोतको अवस्था</h1>
-            {{-- <small>(हाल {{ count($schools)  }}  विद्यालय {{ count($schools) > 1 ? 'हरु छन्' : 'छ' }} )</small> --}}
-            
-        </div>
-        <div class="card-body">
-            <table class="table table-striped table-hover">
-                <thead>
-                    <tr>
-                        <th>क्र.स.</th>
-                        <th>विवरण</th>
-                        <th>सूचक (प्रतिशत)</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @forelse ($budgetResources as $item)
-                    <tr>
-                        <td>{{ $loop->iteration }}</td>
-                        <td>{{ $item->income}}</td>
-                        <td>{{ $item->price}}</td>
-                        <td>
-                            <a class="action-btn text-primary" href="{{ route('budget-resource.edit', $item) }}"><i class="far fa-edit"></i></a>
-                            <form action="{{ route('budget-resource.destroy', $item) }}" method="post" onsubmit="return confirm('के तपाईँ निश्चित हुनुहुन्छ?')" class="form-inline d-inline">
-                                @csrf
-                                @method('delete')
-                                <button type="submit" class="action-btn text-danger"><i class="far fa-trash-alt"></i></button>
-                            </form>
-                        </td>
-                    </tr>
-                    @empty
-                    <tr class="text-center font-italic">
-                        <td colspan="10">* * डाटाबेसमा कुनै डाटा छैन * *</td>
-                    </tr>
-                    @endforelse
-                </tbody>
-            </table>
-        </div>
-    </div>
-</div>
-
 @endsection
 
 @push('scripts')
-<script>
-    $(function() {
+    <script>
+        $(function() {
 
-        $('#select-province-id').change(function() {
-            filterOptions($(this).val(), '#select-district-id option', 'province-id');
+            $('#select-province-id').change(function() {
+                filterOptions($(this).val(), '#select-district-id option', 'province-id');
+            });
+
         });
-
-    });
-
-</script>
-
+    </script>
 @endpush
-
-
-
-
-
-
-
-
--- resources/views/economicCondition/cooperative/index.blade.php
+++ resources/views/economicCondition/cooperative/index.blade.php
@@ -1,133 +1,147 @@
 @extends('layouts.app')
 
 @section('content')
-
-<div class="container">
-    @include('alerts.all')
-</div>
-<div class="container">
-    <div class="card z-depth-0">
-        <div class="col-12">
-            <label class="col-12 text-center font-weight-bold h4 my-5">प्रदेशमा दर्ता भएका सहकारीहरुको विवरण</label>
-            <hr>
+    <div class="container-fluid">
+        <h3 class="font-weight-bold">प्रदेशमा दर्ता भएका सहकारीहरुको विवरण</h3>
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
+                <li class="breadcrumb-item active" aria-current="page">आर्थिक अवस्था</li>
+                <li class="breadcrumb-item active" aria-current="page">प्रदेशमा दर्ता भएका सहकारीहरुको विवरण</li>
+            </ol>
+        </nav>
+        <div class="container">
+            @include('alerts.all')
         </div>
-        <div class="card-body">
-            <form action="{{$cooperative->id? route('cooperative.update',$cooperative):route('cooperative.store')}}" method="POST" class="form">
-                @csrf
-               @isset($cooperative->id)
-                   @method('PUT')
-               @endisset
-               
-                <div class="form-group">
-                    <label for="input-name">जिल्ला</label>
-                    <select name="district" class="form-control" id="">
-                        <option value="">कृपया जिल्ला चयन गर्नुहोस्</option>
-                        @isset($cooperative->id)
-                        <option value="{{$cooperative->district}}" selected>{{$cooperative->district}}</option>
+        <div class="card z-depth-0">
+            <div class="card-header">
+                <div style="overflow: auto;scrollbar-width: none;">
+                    <div>
+                        <nav class="nav nav-pills" id="pills-tab" role="tablist">
+                            <h4>प्रदेशमा दर्ता भएका सहकारीहरुको विवरण</h4>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card-body">
+                <form
+                    action="{{ $cooperative->id ? route('cooperative.update', $cooperative) : route('cooperative.store') }}"
+                    method="POST" class="form">
+                    @csrf
+                    @isset($cooperative->id)
+                        @method('PUT')
                     @endisset
-                        @foreach ($districts as $item)
-                            <option value="{{$item->name}}">{{$item->name}}</option>
-                        @endforeach
-                    </select>
-                </div>
-                <div class="form-group">
-                    <label for="input-name">संगठन</label>
-                    <select name="org" class="form-control" id="">
-                        <option value="">कृपया संगठन चयन गर्नुहोस्</option>
-                        @isset($cooperative->id)
-                            <option value="{{$cooperative->org}}" selected>{{$cooperative->org}}</option>
-                        @endisset
-                        <option value="बहुउद्देश्यीय">बहुउद्देश्यीय</option>
-                        <option value="कृषि">कृषि</option>
-                        <option value="ऋण तथा बचत">ऋण तथा बचत</option>
-                        <option value="स्वास्थ्य">स्वास्थ्य</option>
-                        <option value="सञ्चार">सञ्चार</option>
-                        <option value="विधुत">विधुत</option>
-                        <option value="जडिबुटी">जडिबुटी</option>
-                        <option value="वतावरण संरक्षण">वतावरण संरक्षण</option>
-                        <option value="प्रकाशन">प्रकाशन</option>
-                        <option value="अन्य">अन्य</option>
-                    </select>
-                </div>
 
-                <div class="form-group">
-                    <label for="input-name">संख्या</label>
-                    <input type="number" name="number" class="form-control" value="{{old('number',$cooperative->number)}}">
-                </div>
-               
-                <div class="form-group">
-                    <button type="submit" class="btn btn-success z-depth-0">{{$cooperative->id? 'Update':'सेभ गर्नुहोस्'}} </button>
-                </div>
-            </form>
+                    <div class="row">
+                        <div class="form-group col-lg-4">
+                            <label for="input-name">जिल्ला</label>
+                            <select name="district" class="form-control" id="">
+                                <option value="">कृपया जिल्ला चयन गर्नुहोस्</option>
+                                @isset($cooperative->id)
+                                    <option value="{{ $cooperative->district }}" selected>{{ $cooperative->district }}
+                                    </option>
+                                @endisset
+                                @foreach ($districts as $item)
+                                    <option value="{{ $item->name }}">{{ $item->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="form-group col-lg-4">
+                            <label for="input-name">संगठन</label>
+                            <select name="org" class="form-control" id="">
+                                <option value="">कृपया संगठन चयन गर्नुहोस्</option>
+                                @isset($cooperative->id)
+                                    <option value="{{ $cooperative->org }}" selected>{{ $cooperative->org }}</option>
+                                @endisset
+                                <option value="बहुउद्देश्यीय">बहुउद्देश्यीय</option>
+                                <option value="कृषि">कृषि</option>
+                                <option value="ऋण तथा बचत">ऋण तथा बचत</option>
+                                <option value="स्वास्थ्य">स्वास्थ्य</option>
+                                <option value="सञ्चार">सञ्चार</option>
+                                <option value="विधुत">विधुत</option>
+                                <option value="जडिबुटी">जडिबुटी</option>
+                                <option value="वतावरण संरक्षण">वतावरण संरक्षण</option>
+                                <option value="प्रकाशन">प्रकाशन</option>
+                                <option value="अन्य">अन्य</option>
+                            </select>
+                        </div>
+
+                        <div class="form-group col-lg-4">
+                            <label for="input-name">संख्या</label>
+                            <input type="number" name="number" class="form-control"
+                                value="{{ old('number', $cooperative->number) }}">
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <button type="submit"
+                            class="btn btn-success z-depth-0">{{ $cooperative->id ? 'Update' : 'सेभ गर्नुहोस्' }}
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+        <div class="my-4"></div>
+
+        <div class="card z-depth-0">
+            <div class="card-header">
+                <h1 class="h3-responsive d-inline-block">प्रदेशमा दर्ता भएका सहकारीहरुको विवरण</h1>
+                {{-- <small>(हाल {{ count($schools)  }}  विद्यालय {{ count($schools) > 1 ? 'हरु छन्' : 'छ' }} )</small> --}}
+
+            </div>
+            <div class="card-body">
+                <table class="table table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th>क्र.स.</th>
+                            <th>जिल्ला</th>
+                            <th>संगठन</th>
+                            <th>संख्या</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse ($cooperatives as $item)
+                            <tr>
+                                <td>{{ $loop->iteration }}</td>
+                                <td>{{ $item->district }}</td>
+                                <td>{{ $item->org }}</td>
+                                <td>{{ $item->number }}</td>
+                                <td>
+                                    <a class="action-btn text-primary" href="{{ route('cooperative.edit', $item) }}"><i
+                                            class="far fa-edit"></i></a>
+                                    <form action="{{ route('cooperative.destroy', $item) }}" method="post"
+                                        onsubmit="return confirm('के तपाईँ निश्चित हुनुहुन्छ?')"
+                                        class="form-inline d-inline">
+                                        @csrf
+                                        @method('delete')
+                                        <button type="submit" class="action-btn text-danger"><i
+                                                class="far fa-trash-alt"></i></button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @empty
+                            <tr class="text-center font-italic">
+                                <td colspan="10">* * डाटाबेसमा कुनै डाटा छैन * *</td>
+                            </tr>
+                        @endforelse
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
-
-    <div class="my-4"></div>
-
-    <div class="card z-depth-0">
-        <div class="card-header">
-            <h1 class="h3-responsive d-inline-block">प्रदेशमा दर्ता भएका सहकारीहरुको विवरण</h1>
-            {{-- <small>(हाल {{ count($schools)  }}  विद्यालय {{ count($schools) > 1 ? 'हरु छन्' : 'छ' }} )</small> --}}
-            
-        </div>
-        <div class="card-body">
-            <table class="table table-striped table-hover">
-                <thead>
-                    <tr>
-                        <th>क्र.स.</th>
-                        <th>जिल्ला</th>
-                        <th>संगठन</th>
-                        <th>संख्या</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @forelse ($cooperatives as $item)
-                    <tr>
-                        <td>{{ $loop->iteration }}</td>
-                        <td>{{ $item->district}}</td>
-                        <td>{{ $item->org}}</td>
-                        <td>{{ $item->number}}</td>
-                        <td>
-                            <a class="action-btn text-primary" href="{{ route('cooperative.edit', $item) }}"><i class="far fa-edit"></i></a>
-                            <form action="{{ route('cooperative.destroy', $item) }}" method="post" onsubmit="return confirm('के तपाईँ निश्चित हुनुहुन्छ?')" class="form-inline d-inline">
-                                @csrf
-                                @method('delete')
-                                <button type="submit" class="action-btn text-danger"><i class="far fa-trash-alt"></i></button>
-                            </form>
-                        </td>
-                    </tr>
-                    @empty
-                    <tr class="text-center font-italic">
-                        <td colspan="10">* * डाटाबेसमा कुनै डाटा छैन * *</td>
-                    </tr>
-                    @endforelse
-                </tbody>
-            </table>
-        </div>
-    </div>
-</div>
-
 @endsection
 
 @push('scripts')
-<script>
-    $(function() {
+    <script>
+        $(function() {
 
-        $('#select-province-id').change(function() {
-            filterOptions($(this).val(), '#select-district-id option', 'province-id');
+            $('#select-province-id').change(function() {
+                filterOptions($(this).val(), '#select-district-id option', 'province-id');
+            });
+
         });
-
-    });
-
-</script>
-
+    </script>
 @endpush
-
-
-
-
-
-
-
-
