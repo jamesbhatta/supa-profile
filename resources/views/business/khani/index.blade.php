6d9a42586d7783566cebc6a20b84aa66a2ef35b3
bishal221973 <bishalchaudhary2018bca@gmail.com>
2022-07-03 13:17:04 +0545

    wrp

--- resources/views/business/khani/index.blade.php
+++ resources/views/business/khani/index.blade.php
@@ -132,11 +132,5 @@
 
         });
     </script>
-    $('select').selectpicker();
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
-    <link rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/css/bootstrap-select.css" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/js/bootstrap-select.min.js"></script>
+   
 @endpush
