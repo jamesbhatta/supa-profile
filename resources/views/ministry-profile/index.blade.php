975aa2da6458a9b218f9b0fbaba80f7c758b5003
sgrkathariya <sgrkathariya@gmail.com>
2022-07-05 13:33:02 +0545

    wrp

--- resources/views/ministry-profile/index.blade.php
+++ resources/views/ministry-profile/index.blade.php
@@ -1,8 +1,11 @@
+@extends('layouts.app')
+
+
 <div class="row d-flex justify-content-center mt-5">
-    <div class="col-md-2 col-lg-2">
-        <img alt="Photo" src="https://media.istockphoto.com/photos/trees-forming-a-heart-picture-id537373196?k=20&m=537373196&s=612x612&w=0&h=Y6zpQNFrhLp9lusVP5xbJ8s6H9i0hOZlQwhhPxHlGXU=" style="width: 250px; height: 300px;" />
+    <div class="col-lg-2 col-md-6  col-sm-2">
+        <img alt="Photo" src="https://media.istockphoto.com/photos/trees-forming-a-heart-picture-id537373196?k=20&m=537373196&s=612x612&w=0&h=Y6zpQNFrhLp9lusVP5xbJ8s6H9i0hOZlQwhhPxHlGXU=" class="col-12" />
     </div>
-    <div class="col-md-2 col-lg-2">
+    <div class="col-md-12 col-lg-2 col-sm-2">
         <h3>Trilochan Bhatta</h3>
         <address>
             Chief Minister of Sudurpaschim,
@@ -18,10 +21,10 @@
             Contact the Chief Minister
         </a>
     </div>
-    <div class="col-md-2 col-lg-2">
-        <img alt="Photo" src="https://media.istockphoto.com/photos/trees-forming-a-heart-picture-id537373196?k=20&m=537373196&s=612x612&w=0&h=Y6zpQNFrhLp9lusVP5xbJ8s6H9i0hOZlQwhhPxHlGXU=" style="width: 250px; height: 300px;" />
+    <div class="col-md-12 col-lg-2 col-sm-2">
+        <img alt="Photo" src="https://media.istockphoto.com/photos/trees-forming-a-heart-picture-id537373196?k=20&m=537373196&s=612x612&w=0&h=Y6zpQNFrhLp9lusVP5xbJ8s6H9i0hOZlQwhhPxHlGXU=" class="col-12" />
     </div>
-    <div class="col-md-2 col-lg-2">
+    <div class="col-md-12 col-lg-2 col-sm-2">
         <h3>Trilochan Bhatta</h3>
         <address>
             Chief Minister of Sudurpaschim,
